baea88d77c3e42289ab6ba91075157d2dfc50dfd
Jens Törnell <jens.tornell@gmail.com>
2019-03-28 12:44:53 +0100

    Added options example

--- options-example.php
+++ options-example.php
@@ -1,16 +1,10 @@
 <?php
-return include __DIR__ . '/options-test.php';
 return [
   'root.url' => 'https://example.com',
   'root.path' => __DIR__,
   'spellcheck' => false,
   'project' => [
     'path' => __DIR__ . '/test',
-    //'css' => 'asda'
-  ],
-  'bar' => [
-    //'top' => false,
-    //'bottom' => false
   ],
   'background_color' => '#000',
   'autosave' => 50,
